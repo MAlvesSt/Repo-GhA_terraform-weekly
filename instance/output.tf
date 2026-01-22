74543d9a51933aecc2569dedc62a8203414efe86
MAlvesSt <malves@stemdo.io>
2026-01-22 17:55:19 +0100

    Delete instance directory

--- instance/output.tf
+++
@@ -1,9 +0,0 @@
-
-# ====================
-#  OUTPUT - Instance
-# ====================
-
-output "ec2_instances" {
-  description = "Mapa de instancias EC2 creadas, con sus atributos principales"
-  value = aws_instance.web
-}
